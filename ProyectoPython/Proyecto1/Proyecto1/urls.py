2059b5b8ccdeed0754b502a7d601855d2af4b9c0
Juan Pablo Luna <jp_luna@hotmail.com>
2024-01-28 09:26:18 -0300

    Plantillas

--- ProyectoPython/Proyecto1/Proyecto1/urls.py
+++ ProyectoPython/Proyecto1/Proyecto1/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from Proyecto1.views import saludo, segunda_vista #Accedo a la vista del módulo y del método importando los mismos
+from Proyecto1.views import probando_template #Accedo a la vista del módulo y del método importando los mismos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('segunda_vista/', segunda_vista),
+    path('probando_template/', probando_template),
     
 ]
--- ProyectoPython/Proyecto1/Proyecto1/views.py
+++ ProyectoPython/Proyecto1/Proyecto1/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 
+def probando_template(request):
+    html = open("C:\\Users\\jp_lu\\OneDrive\\Escritorio\\ProyectoCoder\\Plantillas\\template1.html")
+    plantilla = Template(html.read())
+    html.close()
+    contexto = Context()
+    documento = plantilla.render(contexto)              
+    return HttpResponse(documento)
 
 def saludo(request):
     return HttpResponse("Hola Django-Coder")
